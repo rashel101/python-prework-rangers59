a5af3e9e1b3368725ff3fa4dbaf2c5cb8f718254
Rashel Sariol <rashelsariol@Rashels-MacBook-Pro.local>
2021-03-15 15:17:28 -0400

    second commit

--- main.py
+++ main.py
@@ -45,14 +45,17 @@ def is_consecutive (a_list):
     i=0
     status= True
     while i < len(a_list) - 1:
-        if a_list[i]+ 1== a_list[ i +1]:
+        if a_list[i]+ 1== a_list[ i +1 ]:
             i+= 1
         else :
             status= False
             break 
     print(status)
-is_consecutive([1,2,3,4,6])
+is_consecutive([1,2,3,4,5])
 
-
+def welcome(marval_character):
+    print(F'welcome to {marvel_character} Vision')
+    
+welcome('wanda')
 
   
